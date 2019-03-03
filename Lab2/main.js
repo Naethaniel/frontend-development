b47dbb62dfebd913489ad2981c0a6e327c448a2d
Naethaniel <bljuuzeff@gmail.com>
2019-03-03 11:00:10 +0100

    - added what to do - initial database object

---
+++ Lab2/main.js
@@ -0,0 +1,46 @@
+var Database = {
+    db: [],
+    index: 0
+}
+var tomato = {
+    name: 'Tomato',
+    color: 'Red',
+    size: 'Small',
+    species: 'Solanum lycopersicum',
+    kingdom: 'Plantae',
+    weight: 2,
+    isEdible: true
+}
+
+var potato = {
+    name: 'Potato',
+    color: 'Brown',
+    size: 'Big',
+    species: 'Solanum tuberosum',
+    kingdom: 'Plantae',
+    weight: 10,
+    isEdible: true
+}
+
+var cucumber = {
+    name: 'Cucumber',
+    color: 'Green',
+    size: 'Big',
+    species: 'Cucumis sativus',
+    kingdom: 'Plantae',
+    weight: 5,
+    isEdible: true
+}
+
+var rottenPotato = {
+    name: 'Rotten potato',
+    color: 'Brown',
+    size: 'Big',
+    species: 'Solanum tuberosum',
+    kingdom: 'Plantae',
+    weight: 250,
+    isEdible: false
+}
+
+
+
---
+++ Lab2/zad.md
@@ -0,0 +1,8 @@
+Baza danych(ES5)
+----
+
+1. Stworzenie bazy danych przy użyciu Array i Object. <br/> 
+   Skorzystać typów: Number, String, Boolean, Array, Object
+2. CRUD
+3. Wyszukiwanie
+4. Skorzystać z API Array: forEach, filter, pop itp.
