445c35838c6eb6233a033150c581dee0198d03f1
Naethaniel <bljuuzeff@gmail.com>
2019-04-07 09:15:03 +0200

    - Vegetable class

--- Lab5/src/main.js
+++ Lab5/src/main.js
@@ -53,6 +53,54 @@ class Plant {
 
 }
 
-const aPlant = new Plant('Plant', 'Red', 300, 'species', 'kingdom');
+const aPlant = new Plant(
+    'Plant',
+    'Red',
+    300,
+    'Plant',
+    'Fauna'
+);
 console.log(aPlant.message())
 console.log(aPlant.name)
+
+class Vegetable extends Plant {
+    constructor(name, color, size, species, kingdom, weight, isEdible) {
+        super(name, color, size, species, kingdom)
+        this._weight = weight
+        this._isEdible = isEdible
+    }
+
+    message() {
+        return `Hello, i am Vegetable (${this.name} ${this.color})`
+    }
+
+    get weight() {
+        return this._weight
+    }
+
+    get isEdible() {
+        return this._isEdible
+    }
+
+    set weight(weight) {
+        this._weight = weight
+    }
+
+    set isEdible(isEdible) {
+        this._isEdible = isEdible
+    }
+}
+
+const aVegetable = new Vegetable(
+    'Vegetable',
+    'Green',
+    500,
+    'Vegetable',
+    'Fauna',
+    10,
+    false
+);
+
+console.log(aVegetable.message())
+console.log(aVegetable.weight)
+console.log(aVegetable.isEdible)
