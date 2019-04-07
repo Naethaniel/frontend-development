5dec3e6a15179e9d9135a8acd468a92b8309abf2
Naethaniel <bljuuzeff@gmail.com>
2019-04-07 09:12:12 +0200

    - Plant class

---
+++ Lab5/src/main.js
@@ -0,0 +1,58 @@
+class Plant {
+    constructor(name, color, size, species, kingdom) {
+        this._name = name
+        this._color = color
+        this._size = size
+        this._species = species
+        this._kingdom = kingdom
+    }
+
+    message() {
+        return `Hello, i am Plant (${this.species} ${this.kingdom})`
+    }
+
+    get name() {
+        return this._name
+    }
+
+    get color() {
+        return this._color
+    }
+
+    get size() {
+        return this._size
+    }
+
+    get species() {
+        return this._species
+    }
+
+    get kingdom() {
+        return this._kingdom
+    }
+
+    set name(name) {
+        this._name = name
+    }
+
+    set color(color) {
+        this._color = color
+    }
+
+    set size(size) {
+        this._size = size
+    }
+
+    set species(species) {
+        this._species = species
+    }
+
+    set kingdom(kingdom) {
+        this._kingdom = kingdom
+    }
+
+}
+
+const aPlant = new Plant('Plant', 'Red', 300, 'species', 'kingdom');
+console.log(aPlant.message())
+console.log(aPlant.name)
