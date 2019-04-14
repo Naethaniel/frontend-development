8a80bdb171945e411476bc61008b8ba1f1c4c643
Naethaniel <bljuuzeff@gmail.com>
2019-04-14 09:26:40 +0200

    - Hello World in React

---
+++ Lab6/reactapp/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function Hello() {
+    return (
+        <div>Hello world!</div>
+    )
+}
+
+ReactDOM.render(<Hello/>, document.getElementById('root'))
