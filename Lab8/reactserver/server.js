ddadbf0d6362610796a20e265bf6dd698f07234f
Naethaniel <bljuuzeff@gmail.com>
2019-05-24 12:17:34 +0200

    add nodemon and npm run start to package.json, update /api/vegetables to accept parameter to filter results

--- Lab8/reactserver/server.js
+++ Lab8/reactserver/server.js
@@ -8,14 +8,25 @@ const port = 4000;
 const db = new Database();
 app.use(bodyParser.json());
 
+
+// Vegetables
 app.get('/api/vegetables', (req, res) => {
-    res.send({vegetables: db._db});
-})
+    const property = Object.keys(req.query)[0];
+    if (!property) {
+        res.status(200);
+        res.send({vegetables: db._db});
+    } else {
+        const vegetables = db.findVegetablesByProperty(req.query[property], property);
+        res.status(200);
+        res.send({vegetables})
+    }
+});
 
 app.post('/api/vegetables', (req, res) => {
     db.addVegetable(req.body);
     res.status(201);
-    res.send(request.body)
-})
+    res.send(req.body)
+});
+
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
